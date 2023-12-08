11f26a0fd7717b933be0935bb68ee04ecefe83d2
GitHub Action <action@github.com>
2023-12-08 03:18:31 +0000

    Template cleanup

--- src/main/kotlin/com/github/coderl98/json5generate/MyBundle.kt
+++ src/main/kotlin/com/github/coderl98/json5generate/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.coderl98.json5generate
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/coderl98/json5generate/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/coderl98/json5generate/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.coderl98.json5generate.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
